fc8eb7da4dbe759fe6d8cd67776591f61399878c
LiZimo <zimoli@uchicago.edu>
2021-01-08 18:56:22 -0800

    a

--- util/resize_script.py
+++ util/resize_script.py
@@ -4,8 +4,8 @@ import ntpath
 import cv2
 
 outsize = 512
-#in_dir = '/mount/Users/zli/stylegan2_data/raw_images/capture_data/'
-in_dir = '/vgldb1/LightStageFaceDB/Datasets/FaceEncoding/DiffuseAlbedo'
+in_dir = '/mount/Users/zli/stylegan2_data/raw_images/capture_data/DiffuseAlbedo'
+#in_dir = '/vgldb1/LightStageFaceDB/Datasets/FaceEncoding/DiffuseAlbedo'
 out_dir = '/mount/Users/zli/stylegan2_data/raw_images/capture_data/texture_completion_dataset'
 
 all_files = glob.glob(in_dir + '/*.exr')
