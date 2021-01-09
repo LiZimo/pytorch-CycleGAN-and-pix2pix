ad36201dac0307074d0bdea565a3177d3e1f2248
LiZimo <zimoli@uchicago.edu>
2021-01-09 01:34:19 -0800

    a

--- data/completion_dataset.py
+++ data/completion_dataset.py
@@ -50,7 +50,7 @@ class CompletionDataset(BaseDataset):
         full = torch.clip(full, 0. ,1,)
         full = self.exr2rgb(full)
         mean_color = torch.mean(full, dim = [0,1])
-        mean_color_mat = torch.repeat(mean_color,512,512,1)
+        #mean_color_mat = torch.repeat(mean_color,512,512,1)
 
         partial = torch.clone(full)
         random_mask_id = random.randint(0, len(self.mask_path) - 1)
@@ -88,7 +88,7 @@ class CompletionDataset(BaseDataset):
         transform_params = get_params(self.opt, (A.shape[0], A.shape[1]))
         #print(transform_params)
         #transform_params = get_params(self.opt, (A.shape[0], A.shape[1]))
-        A_transform = get_transform(self.opt, transform_params, num_channels = 3, grayscale=(self.input_nc == 1), convert = False)
+        A_transform = get_transform(self.opt, transform_params, num_channels = 4, grayscale=(self.input_nc == 1), convert = False)
         B_transform = get_transform(self.opt, transform_params, grayscale=(self.output_nc == 1), convert = False)
 
         A = A_transform(A)
