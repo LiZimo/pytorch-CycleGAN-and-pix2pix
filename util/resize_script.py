1c6a9c2521145ae0781e1b271d8592506524f80b
LiZimo <zimoli@uchicago.edu>
2021-01-08 18:54:16 -0800

    a

--- util/resize_script.py
+++ util/resize_script.py
@@ -10,7 +10,7 @@ out_dir = '/mount/Users/zli/stylegan2_data/raw_images/capture_data/texture_compl
 all_files = glob.glob(in_dir + '/*.exr')
 counter = 0
 for imgname in all_files:
-	img = imageio.imread(imagename)
+	img = imageio.imread(imgname)
 
 	if img.shape[0] != out_size:
 		out_im = cv2.resize(img, (outsize,outsize))
