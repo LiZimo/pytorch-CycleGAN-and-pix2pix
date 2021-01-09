88d9dc5a6502e105366bff63644484dd995f3f2c
LiZimo <zimoli@uchicago.edu>
2021-01-08 22:48:53 -0800

    a

--- util/visualizer.py
+++ util/visualizer.py
@@ -119,6 +119,8 @@ class Visualizer():
                 images = []
                 idx = 0
                 for label, image in visuals.items():
+                    print(image.shape)
+                    print('image shape')
                     image = image[:,:,0:3]
                     image_numpy = util.tensor2im(image)
                     label_html_row += '<td>%s</td>' % label
