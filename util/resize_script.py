814c46bea181e6a2d057410e09f3c47d9da7dc6d
LiZimo <zimoli@uchicago.edu>
2021-01-08 21:19:55 -0800

    a

--- util/resize_script.py
+++ util/resize_script.py
@@ -5,7 +5,7 @@ import cv2
 import os
 
 outsize = 512
-in_dir = '/mount/Users/zli/stylegan2_data/raw_images/capture_data/DiffuseAlbedo'
+in_dir = '/mount/Users/zli/stylegan2_data/raw_images/capture_data/AlbedoAugmentation_TG'
 #in_dir = '/vgldb1/LightStageFaceDB/Datasets/FaceEncoding/DiffuseAlbedo'
 out_dir = '/mount/Users/zli/stylegan2_data/raw_images/capture_data/texture_completion_dataset'
 
