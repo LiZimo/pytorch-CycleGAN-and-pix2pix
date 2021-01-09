d982b9708d5e0e0c44d28d5ba762eecf4e1c7f8c
LiZimo <zimoli@uchicago.edu>
2021-01-09 00:20:31 -0800

    a

--- data/completion_dataset.py
+++ data/completion_dataset.py
@@ -64,6 +64,7 @@ class CompletionDataset(BaseDataset):
 
         #partial = mask
         #A = torch.cat((partial, partial[:,:,0:1]), 2)
+        A = partial
         A = A.permute(2,0,1)
         full = full.permute(2,0,1)
         #A = Image.fromarray(A, mode = 'RGBA')
