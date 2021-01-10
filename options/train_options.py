767d6af6d5cc2dea2ee44ddb044e7cc914831aa4
LiZimo <zimoli@uchicago.edu>
2021-01-09 17:38:03 -0800

    a

--- options/train_options.py
+++ options/train_options.py
@@ -35,7 +35,7 @@ class TrainOptions(BaseOptions):
         parser.add_argument('--pool_size', type=int, default=50, help='the size of image buffer that stores previously generated images')
         parser.add_argument('--lr_policy', type=str, default='linear', help='learning rate policy. [linear | step | plateau | cosine]')
         parser.add_argument('--lr_decay_iters', type=int, default=50, help='multiply by a gamma every lr_decay_iters iterations')
-        parser.add_argument('--mask_path', type=str, default='/mount/Users/zli/stylegan2_data/raw_images/combined_masks', help='directory of masks')
+        
 
         self.isTrain = True
         return parser
