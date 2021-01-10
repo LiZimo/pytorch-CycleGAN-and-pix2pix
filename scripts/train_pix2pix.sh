ccde23c1b6d1d97bb830060fc816be8065f78aa3
LiZimo <zimoli@uchicago.edu>
2021-01-09 17:36:26 -0800

    a

--- scripts/train_pix2pix.sh
+++ scripts/train_pix2pix.sh
@@ -1,2 +1,7 @@
 set -ex
 python train.py --dataroot /mount/Users/zli/stylegan2_data/raw_images/capture_data/ --gpu_ids 0,1,2,3,4 --name completion_pix2pix --model pix2pix --netG unet_256 --direction AtoB --lambda_L1 400 --dataset_mode completion --norm batch --pool_size 0 --no_flip 
+
+
+## testing
+
+python test.py --dataroot /mount/Users/zli/stylegan2_data/raw_images/capture_data/ --name completion_pix2pix --model pix2pix --netG unet_256 --direction AtoB --dataset_mode completion --norm batch
