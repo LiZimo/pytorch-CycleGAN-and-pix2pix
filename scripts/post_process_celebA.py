ecf88223c398864c924e21dd9970e940da3de895
LiZimo <zimoli@uchicago.edu>
2021-01-10 16:34:10 -0800

    a

--- scripts/post_process_celebA.py
+++ scripts/post_process_celebA.py
@@ -6,19 +6,24 @@ import ntpath
 import numpy as np
 
 input_dir = '/mount/Users/zli/pix2pix_data/completion_pix2pix_instance/test_latest/images'
+original_dir = 'mount/Users/zli/stylegan2_data/raw_images/capture_data/test'
 output_dir = '/mount/Users/zli/pix2pix_data/completion_pix2pix_instance/test_latest/post_process'
 
 network_outputs = glob.glob(input_dir + '/*fake_B.png')
 
 for imgname in network_outputs:
-	input_img = imageio.imread(imgname.replace('fake', 'real'))
+	basename = ntpath.basename(imgname).replace('_fake_B','')
+	input_img = imageio.imread(os.path.join(original_dir, basename))
 	network_img = imageio.imread(imgname)
+	network_img = cv2.resize(network_img, (384,384), interpolation=cv2.INTER_NEAREST)
+	empty_im = np.zeros((512,512,3))
+	empty_im[slack:-slack,slack:-slack,:] = network_img
 
-	input_img[input_img == 0] = network_img[input_img == 0]
+	input_img[input_img == 0] = empty_im[input_img == 0]
 	slack = 64
 	empty_im = np.zeros((512,512,3))
 	resized = cv2.resize(input_img, (384,384), interpolation=cv2.INTER_NEAREST)
 	empty_im[slack:-slack,slack:-slack,:] = resized
 
-	out_name = os.path.join(output_dir, ntpath.basename(imgname).replace('_fake_B',''))
+	out_name = os.path.join(output_dir, basename)
 	imageio.imwrite(out_name, empty_im)
