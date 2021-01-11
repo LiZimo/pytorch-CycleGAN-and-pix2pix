80eb66107a7e1fdab357d7fa68696aed6a4df789
LiZimo <zimoli@uchicago.edu>
2021-01-10 16:56:28 -0800

    a

--- scripts/post_process_celebA.py
+++ scripts/post_process_celebA.py
@@ -23,7 +23,8 @@ for imgname in network_outputs:
 	empty_im[slack:-slack,slack:-slack,:] = network_img
 
 	input_img[input_img == 0] = empty_im[input_img == 0]
-	template = imageio.imread(template)
+	template = imageio.imread(template)[:,:,0:3]
+	template = (template!=0).astype(np.float32)
 	print(input_img.shape)
 	print(template.shape)
 	input_img[template == 0] = 0
