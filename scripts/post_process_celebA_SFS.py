fd0c5e4dd01b898ac205f747b3ae960d7668d9c8
LiZimo <zimoli@uchicago.edu>
2021-01-20 00:07:21 -0800

    a

--- scripts/post_process_celebA_SFS.py
+++ scripts/post_process_celebA_SFS.py
@@ -42,6 +42,7 @@ for imgname in images:
 	hair_mask_name = hair_mask_dir + '/' + basename
 	# if os.path.exists(hair_mask_name):
 	input_hair_mask = np.array(Image.open(hair_mask_name).convert('1')).astype(np.float32)
+	input_hair_mask = cv2.resize(input_hair_mask, (512,512))
 	# else:
 	# 	input_hair_mask = np.zeros(template.shape)
 	reverse_hair_mask = 1. - input_hair_mask
